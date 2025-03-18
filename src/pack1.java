47926b3a9e08dbdb77b30eaf806ed70fc0a022b5
FrogBlaster123 <kaustubh.kantekar@gmail.com>
2025-03-18 23:52:35 +0530

    Second Commit

---
+++ src/pack1.java
@@ -0,0 +1,10 @@
+package ee_java;
+public class pack1{
+	int a;
+	public int b=10;
+	private int c = 5;
+	protected int d;
+	public void add_m1(){
+		System.out.println("Sum is: " + (a + b + c + d));
+	}
+}
---
+++ src/packtest.java
@@ -0,0 +1,8 @@
+​import ee_java.*;
+
+class packtest{
+	public static void main(String[] args){
+		pack1 obj = new pack1();
+		obj.add_m1();
+	}
+}
